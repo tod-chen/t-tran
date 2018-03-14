5def1b9719516be04b49571862bf76913a050a6f
tod-chen <tod-chen@foxmail.com>
2018-03-14 22:06:30 +0800

    Add files via upload

---
+++ IUser.go
@@ -0,0 +1,21 @@
+package main
+
+// IUser 用户行为接口
+type IUser interface{
+	// 查询车次及余票数
+	queryTrans()
+	// 提交订单
+	submitOrder()
+	// 查询订单
+	queryOrder()
+	// 取消订单
+	cancelOrder()
+	// 支付订单
+	paymentOrder()
+	// 退票
+	refundOrder()
+	// 改签
+	changeOrder()
+	// 取票
+	printTicket()
+}
